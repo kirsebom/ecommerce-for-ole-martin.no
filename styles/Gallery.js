7db66d1b311edc208a1c657417ee7555f0264bd1
kirsebom <59082697+kirsebom@users.noreply.github.com>
2022-07-31 11:37:55 +0200

    Added a Loadingspinner

--- styles/Gallery.js
+++ styles/Gallery.js
@@ -7,3 +7,36 @@ export const Gallery = styled.div`
 		grid-template-columns: repeat(auto-fit, minmax(15rem, 1fr));
 	}
 `;
+
+export const SpinnerContainer = styled.div`
+	display: flex;
+	width: 100%;
+	height: 80vh;
+	justify-content: center;
+	align-items: center;
+`;
+
+export const Spinner = styled.div`
+	display: inline-block;
+	width: 80px;
+	height: 80px;
+	&::after {
+		content: " ";
+		display: block;
+		width: 64px;
+		height: 64px;
+		margin: 8px;
+		border-radius: 50%;
+		border: 6px solid #000;
+		border-color: #000 transparent #000 transparent;
+		animation: spinnerAnimation 1.2s linear infinite;
+	}
+	@keyframes spinnerAnimation {
+		0% {
+			transform: rotate(0deg);
+		}
+		100% {
+			transform: rotate(360deg);
+		}
+	}
+`;
